7b604773070f532dd455bad0f44744d94d85ecb0
Fionnán O Cualáin <Fionnan98@gmail.com>
2025-04-29 12:34:27 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/',
+  base: '/finneas98.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
